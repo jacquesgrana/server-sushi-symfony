3edf7e44d468140ae98c1fda5cd9e3dff05caa51
jacques <jac.granarolo.info@gmail.com>
2025-07-05 18:20:10 +0200

    endpoint /carousel/get_slides ok

--- src/Controller/CarouselController.php
+++ src/Controller/CarouselController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PhotoSlideRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class CarouselController extends AbstractController
 {
     #[Route('/get_slides', name: 'app_carousel_get_slides', methods: ['GET'])]
-    public function index(): JsonResponse
+    public function index(PhotoSlideRepository $photoSlideRepository): JsonResponse
     {
+        $slides = $photoSlideRepository->findAll();
+        $data = [];
+        foreach ($slides as $slide) {
+                $data[] = [
+                    'id' => $slide->getId(),
+                    'image' => $slide->getImage(),
+                    'alt' => $slide->getAlt(),
+                    'title' => $slide->getTitle(),
+                    'description' => $slide->getDescription()
+                ];
+            }
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CarouselController.php',
-        ]);
+            'success' => true,
+            'message' => 'PhotoSlide listed successfully',
+            'data' => $data], 200);
     }
 }
