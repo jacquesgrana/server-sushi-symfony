558d86ae1caef4a2fd22c05dd984d3ac0659df37
jacques <jac.granarolo.info@gmail.com>
2025-07-21 14:24:49 +0200

    modify last method create prospect : add email verification if is already used

--- src/Controller/ContactFormProspectController.php
+++ src/Controller/ContactFormProspectController.php
@@ -32,15 +32,9 @@ final class ContactFormProspectController extends AbstractController
     #[Route('/api/contact-form-prospect/create/{id}', name: 'app_contact_form_prospect_api_create', methods: ['POST'])]
     public function createProspectFromContactForm(
         ContactForm $contactForm,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        ContactFormProspectRepository $contactFormProspectRepository
     ) {
-        if(!$contactForm){
-            return $this->json([
-                'success' => false,
-                'message' => 'ContactForm not found',
-                'data' => []
-            ], 404);
-        }
 
         if ($contactForm->getContactFormProspect()) {
             return $this->json([
@@ -49,23 +43,46 @@ final class ContactFormProspectController extends AbstractController
                 'data' => []
             ], 400);
         }
-        
-        $contactFormProspect = new ContactFormProspect();
-        $contactFormProspect->setName($contactForm->getName());
-        $contactFormProspect->setFirstName($contactForm->getFirstName());
-        $contactFormProspect->setEmail($contactForm->getEmail());
-        $contactFormProspect->setPhone($contactForm->getPhone());
-        $contactFormProspect->setComment("Commentaire à modifier.");
-        $contactFormProspect->setDate(new \DateTime());
-        $contactFormProspect->addContactForm($contactForm);
-        $entityManager->persist($contactFormProspect);
-        $entityManager->flush();
 
-        return $this->json([
-            'success' => true,
-            'message' => 'ContactFormProspect created successfully',
-            'data' => $contactFormProspect->normalize()
-        ], 200);
+        // TODO vérifier si l'email n'est pas déjà utilisé par un autre Prospect
+        //$prospects = $contactFormProspectRepository->findBy(['email' => $contactForm->getEmail()]);
+            
+        $oldProspect = $contactFormProspectRepository->findOneBy(['email' => $contactForm->getEmail()]);
+
+        // si oui ajouter le ContactForm au Prospect
+        if ($oldProspect) {
+            $oldProspect->addContactForm($contactForm);
+            //$entityManager->persist($oldProspect);
+            $entityManager->flush();
+
+            return $this->json([
+                'success' => true,
+                'message' => 'ContactForm added to Prospect successfully',
+                'data' => $oldProspect->normalize()
+            ], 200);
+        }
+
+        // sinon créer un nouveau Prospect
+        else {
+            $contactFormProspect = new ContactFormProspect();
+            $contactFormProspect->setName($contactForm->getName());
+            $contactFormProspect->setFirstName($contactForm->getFirstName());
+            $contactFormProspect->setEmail($contactForm->getEmail());
+            $contactFormProspect->setPhone($contactForm->getPhone());
+            $contactFormProspect->setComment("Commentaire à modifier.");
+            $contactFormProspect->setDate(new \DateTime());
+            $contactFormProspect->addContactForm($contactForm);
+            $entityManager->persist($contactFormProspect);
+            $entityManager->flush();
+
+            return $this->json([
+                'success' => true,
+                'message' => 'ContactFormProspect created successfully',
+                'data' => $contactFormProspect->normalize()
+            ], 200);
+        }
+
+        
         
     }
 }
