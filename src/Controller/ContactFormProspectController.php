b061393f285445eed417c76d54d24074e9ebb8a2
jacques <jac.granarolo.info@gmail.com>
2025-07-21 13:33:30 +0200

    add endpoint to create a prospect from a contact form

--- src/Controller/ContactFormProspectController.php
+++ src/Controller/ContactFormProspectController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactForm;
+use App\Entity\ContactFormProspect;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,5 +27,45 @@ final class ContactFormProspectController extends AbstractController
             'message' => 'ContactFormProspect listed successfully',
             'data' => $data
         ], 200);
+    }
+
+    #[Route('/api/contact-form-prospect/create/{id}', name: 'app_contact_form_prospect_api_create', methods: ['POST'])]
+    public function createProspectFromContactForm(
+        ContactForm $contactForm,
+        EntityManagerInterface $entityManager
+    ) {
+        if(!$contactForm){
+            return $this->json([
+                'success' => false,
+                'message' => 'ContactForm not found',
+                'data' => []
+            ], 404);
+        }
+
+        if ($contactForm->getContactFormProspect()) {
+            return $this->json([
+                'success' => false,
+                'message' => 'ContactForm already has a Prospect',
+                'data' => []
+            ], 400);
+        }
+        
+        $contactFormProspect = new ContactFormProspect();
+        $contactFormProspect->setName($contactForm->getName());
+        $contactFormProspect->setFirstName($contactForm->getFirstName());
+        $contactFormProspect->setEmail($contactForm->getEmail());
+        $contactFormProspect->setPhone($contactForm->getPhone());
+        $contactFormProspect->setComment("Commentaire à modifier.");
+        $contactFormProspect->setDate(new \DateTime());
+        $contactFormProspect->addContactForm($contactForm);
+        $entityManager->persist($contactFormProspect);
+        $entityManager->flush();
+
+        return $this->json([
+            'success' => true,
+            'message' => 'ContactFormProspect created successfully',
+            'data' => $contactFormProspect->normalize()
+        ], 200);
+        
     }
 }
